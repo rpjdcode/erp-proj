0e084d91281e004f2495516d4b2243386e3be7c8
rpjcode <69083318+rpjdcode@users.noreply.github.com>
2024-10-19 11:41:55 +0100

    Modificación entidades

---
+++ src/main/java/es/rpjd/app/hibernate/entity/Product.java
@@ -0,0 +1,77 @@
+package es.rpjd.app.hibernate.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Table(name = "PRODUCT")
+@Entity
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID", columnDefinition = "INT(11)", length = 11, nullable = false)
+	private Long id;
+	
+	@Column(name = "NAME", columnDefinition = "VARCHAR(90)", nullable = false, unique = true)
+	private String name;
+	
+	@Column(name = "PRODUCT_CODE", columnDefinition = "CHAR(10)", nullable = false, unique = true)
+	private String productCode;
+	
+	@Column(name = "PRICE", columnDefinition = "DECIMAL(19,2)", precision = 19, scale = 2)
+	private BigDecimal price;
+	
+	@ManyToOne
+	@JoinColumn(name = "PRODUCT_TYPE", columnDefinition = "INT(11)")
+	private ProductType productType;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getProductCode() {
+		return productCode;
+	}
+
+	public void setProductCode(String productCode) {
+		this.productCode = productCode;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	
+	public ProductType getProductType() {
+		return productType;
+	}
+	
+	public void setProductType(ProductType productType) {
+		this.productType = productType;
+	}
+	
+	
+}
--- src/main/java/es/rpjd/app/hibernate/entity/ProductType.java
+++ src/main/java/es/rpjd/app/hibernate/entity/ProductType.java
@@ -1,39 +1,44 @@
 package es.rpjd.app.hibernate.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
-@Table(name = "USER")
+@Table(name = "PRODUCT_TYPE")
 @Entity
-public class User {
+public class ProductType {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ID", columnDefinition = "INT(11)", length = 11, nullable = false)
 	private Long id;
+
+	@Column(name = "TYPE", columnDefinition = "VARCHAR(30)", length = 30, nullable = false, unique = true)
+	private String type;
 	
-	@Column(name = "USERNAME", columnDefinition = "VARCHAR(20)", length = 20, unique = true, nullable = false)
-	private String username;
-	
+	@OneToMany(mappedBy = "productType")
+	private List<Product> products;
+
 	public Long getId() {
 		return id;
 	}
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
-	public String getUsername() {
-		return username;
+
+	public String getType() {
+		return type;
 	}
-	
-	public void setUsername(String username) {
-		this.username = username;
+
+	public void setType(String type) {
+		this.type = type;
 	}
-	
-	
+
 }
