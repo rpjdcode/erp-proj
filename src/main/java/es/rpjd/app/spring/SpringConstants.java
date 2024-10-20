795dad17d6ef7ff2570351f48cf5d7bfb7225646
rpjcode <69083318+rpjdcode@users.noreply.github.com>
2024-10-20 22:53:09 +0100

    Dependencia JavaFX-Swing para ScenicView

--- src/main/java/es/rpjd/app/spring/SpringConstants.java
+++ src/main/java/es/rpjd/app/spring/SpringConstants.java
@@ -2,14 +2,15 @@ package es.rpjd.app.spring;
 
 public class SpringConstants {
 
+	private SpringConstants() {
+	}
+
 	/*
 	 * Nombres de Beans relacionados a controladores de JavaFX
 	 */
 	public static final String BEAN_CONTROLLER_ROOT = "rootController";
 	public static final String BEAN_CONTROLLER_TESTING = "testingController";
 
-	
-	
 	/*
 	 * Nombres de Beans relacionados con servicios
 	 */
