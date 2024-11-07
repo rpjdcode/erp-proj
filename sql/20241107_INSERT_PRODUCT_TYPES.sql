8309fbb9c30ffa36c6a820d24c60e7969774ad0a
rpjcode <69083318+rpjdcode@users.noreply.github.com>
2024-11-07 21:13:18 +0000

    Inserciones de tipos de producto (SQL)

---
+++ sql/20241107_INSERT_PRODUCT_TYPES.sql
@@ -0,0 +1,6 @@
+INSERT INTO JAVAFX_APP.PRODUCT_TYPE(TYP_CODE, PROPERTY_NAME) VALUES
+('VEG', 'app.lang.product.type.veg'),
+('CAR', 'app.lang.product.type.car'),
+('OCE', 'app.lang.product.type.ocean'),
+('DES', 'app.lang.product.type.dessert'),
+('DRI', 'app.lang.product.type.drink');
